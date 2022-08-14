fe1edd60bc39bc39b81e14d853a17a5cb8329a52
NguyenBangPhi <bangphin1@gmail.com>
2022-08-14 12:17:16 +0700

    Upload source

---
+++ src/main/java/com/shop/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+	
+	@RequestMapping("/product")
+	public String product() {
+		return "product/index";
+	}
+}
